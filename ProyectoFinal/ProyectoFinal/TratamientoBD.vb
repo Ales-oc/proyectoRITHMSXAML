19d28eeb2248b351ff77738ac92c6d57e87aa6b3
Ale Or <aocaraballo19@iespuntadelverde.es>
2021-02-20 03:56:51 +0100

    Algoritmo Inicio sesión terminado

---
+++ ProyectoFinal/ProyectoFinal/TratamientoBD.vb
@@ -0,0 +1,38 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+
+Public Class TratamientoBD
+    Public Function abrirConexion(ByVal cadenaConexion As String)
+        Dim conexion As SqlConnection = New SqlConnection(cadenaConexion)
+        conexion.Open()
+        Return conexion
+    End Function
+
+    Public Sub cerrarConexion(ByVal conexion As SqlConnection)
+        If Not IsNothing(conexion) Then
+            conexion.Close()
+        End If
+    End Sub
+
+    Public Function comprobarRegistroExistente(ByVal cadenaConexion As String, ByVal selectQuery As String)
+        Dim conexion As SqlConnection = New SqlConnection(cadenaConexion)
+        Dim adapter As SqlDataAdapter = New SqlDataAdapter(selectQuery, conexion)
+        Dim dt As DataTable = New DataTable
+
+        adapter.Fill(dt)
+        If dt.Rows.Count > 0 Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+    Public Function obtenerDatos(ByVal cadenaConexion As String, ByVal selectQuery As String)
+        Dim conexion As SqlConnection = New SqlConnection(cadenaConexion)
+        Dim adapter As SqlDataAdapter = New SqlDataAdapter(selectQuery, conexion)
+        Dim ds As DataSet = New DataSet
+
+        adapter.Fill(ds)
+        Return ds
+    End Function
+End Class
---
+++ ProyectoFinal/creacionDBRithms.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE Rithms
+USE Rithms
+
+CREATE TABLE Usuarios(
+	id_user INT IDENTITY(1,1) PRIMARY KEY,
+	nickname VARCHAR(50) UNIQUE,
+	correo VARCHAR(80) UNIQUE,
+	password VARBINARY(8000),
+	fecha_nacimiento DATETIME
+);
+
+INSERT INTO Usuarios VALUES
+('admin_aoc', 'alejandroortizcaraballo@gmail.com', ENCRYPTBYPASSPHRASE('password', 'admin_aoc'), '14-02-2021');
+
+CREATE TABLE Canciones(
+	id_song INT IDENTITY(1,1) PRIMARY KEY,
+	autor VARCHAR(60),
+	titulo NVARCHAR(80),
+	genero NVARCHAR(50),
+	archivo VARBINARY(MAX)
+);
+
+CREATE TABLE Playlists(
+	id_playlist INT IDENTITY(1,1) PRIMARY KEY,
+	titulo VARCHAR(80),
+	id_user INT,
+	CONSTRAINT FK_idUsuario FOREIGN KEY (id_user)
+	REFERENCES Usuarios(id_user) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE Canc_Playl(
+	id_song INT,
+	id_playlist INT,
+	fecha_alta DATETIME,
+	PRIMARY KEY(id_song, id_playlist),
+	CONSTRAINT FK_idSong FOREIGN KEY (id_song)
+	REFERENCES Canciones(id_song) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT FK_idPlaylist FOREIGN KEY (id_playlist)
+	REFERENCES Playlists(id_playlist) ON DELETE CASCADE ON UPDATE CASCADE
+);
